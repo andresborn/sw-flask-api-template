99554fa9d27fbdc91b2ea4fec2d381199f92a295
andresborn <andres.born@gmail.com>
2021-03-02 20:24:10 +0000

    Edited user model

--- src/models.py
+++ src/models.py
@@ -8,11 +8,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     favorites = db.relationship("Favorite", backref="user", lazy=True)
 
-    def __init__(self, username, email):
+    def __init__(self, username, email, password):
         self.username = username
         self.email = email
+        self.password = password
 
     def __repr__(self):
         return "<User %r>" % self.username
